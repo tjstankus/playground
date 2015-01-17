e3401b202905a42bf236c2abca383411c2af31f2
TJ Stankus <tjstankus@gmail.com>
2015-01-16 20:01:04 -0500

    Pull trello config from ENV

---
+++ trello/trello.rb
@@ -0,0 +1,8 @@
+require 'dotenv'
+Dotenv.load
+require 'trello'
+
+Trello.configure do |config|
+  config.developer_public_key = ENV['TRELLO_PUBLIC_KEY']
+  config.member_token = ENV['TRELLO_MEMBER_TOKEN']
+end
