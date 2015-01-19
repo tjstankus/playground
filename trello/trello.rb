5d8b3d9a50b5645eb00fde686ccb3fc781071703
TJ Stankus <tjstankus@gmail.com>
2015-01-18 23:39:52 -0500

    Hacking around using ruby-trello

--- trello/trello.rb
+++ trello/trello.rb
@@ -1,8 +1,19 @@
 require 'dotenv'
-Dotenv.load
+dotenv_path = File.expand_path(File.join(File.dirname(__FILE__), '.env'))
+Dotenv.load(dotenv_path)
 require 'trello'
 
 Trello.configure do |config|
   config.developer_public_key = ENV['TRELLO_PUBLIC_KEY']
   config.member_token = ENV['TRELLO_MEMBER_TOKEN']
 end
+
+boards = Trello::Board.all
+board = boards.first
+list = board.lists.first
+cards = list.cards
+card = cards.first
+puts card.inspect
+puts card.checklists.first.check_items.collect { |item| item['name'] }
+puts cards.map(&:name).inspect
+
